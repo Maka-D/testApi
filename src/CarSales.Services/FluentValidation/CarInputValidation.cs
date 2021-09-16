f047f9035f44903090af8ceb2000de1d79b44218
Maka <maka.dabaxishvili@gmail.com>
2021-09-17 03:54:15 +0400

    update

---
+++ src/CarSales.Services/FluentValidation/CarInputValidation.cs
@@ -0,0 +1,45 @@
+﻿using CarSales.Services.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSales.Services.FluentValidation
+{
+    public class CarInputValidation : AbstractValidator<CarInput>
+    {
+        public CarInputValidation()
+        {
+            RuleFor(x => x.VinCode)
+                .NotEmpty()
+                .Length(17);
+
+            RuleFor(x => x.Brand)
+                .NotEmpty()
+                .MaximumLength(20);
+
+            RuleFor(x => x.Model)
+                .NotEmpty()
+                .MaximumLength(20);
+
+            RuleFor(x => x.CarNumber)
+                .NotEmpty()
+                .MaximumLength(15);
+
+            RuleFor(x => x.Price)
+                .NotEmpty();
+
+            RuleFor(x => x.ReleaseDate)
+                .NotEmpty();
+
+            RuleFor(x => x.StartedSale)
+               .NotEmpty();
+
+            RuleFor(x => x.FinishedSale)
+               .NotEmpty();
+
+        }
+    }
+}
---
+++ src/CarSales.Services/FluentValidation/ClientInputValidation.cs
@@ -0,0 +1,38 @@
+﻿using CarSales.Services.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSales.Services.FluentValidator
+{
+    public class ClientInputValidation : AbstractValidator<ClientInput>
+    {
+        public ClientInputValidation()
+        {
+            RuleFor(x => x.IdentityNumber)
+                .NotEmpty()
+                .Length(11);
+
+            RuleFor(x => x.FirstName)
+                .MaximumLength(20);
+
+            RuleFor(x => x.SecondName)
+                .MaximumLength(30);
+
+            RuleFor(x => x.Email)
+                .EmailAddress();
+
+            RuleFor(x => x.Address)
+                .MaximumLength(50);
+
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty();
+                
+                
+                
+        }
+    }
+}
